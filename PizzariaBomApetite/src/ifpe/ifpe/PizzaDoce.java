daf0b2843758ce913d7f9df01c0a4ddd0579f87b
carlla15 <carllapatricia25@gmail.com>
2023-11-09 09:38:08 -0300

    Metodo Bordas recheadas adicionado!

--- PizzariaBomApetite/src/ifpe/ifpe/PizzaDoce.java
+++ PizzariaBomApetite/src/ifpe/ifpe/PizzaDoce.java
@@ -1,6 +1,7 @@
 package ifpe;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class PizzaDoce extends Pizza {
 	
@@ -35,6 +36,24 @@ public class PizzaDoce extends Pizza {
 	public void setPreco(double preco) {
 		this.preco = preco;
 	}
+	
+	public void adicionarBordasRecheadas() {
+		// TODO Auto-generated method stub
+		Scanner perguntarCliente = new Scanner(System.in);
 
+        System.out.println("Deseja adicionar bordas recheadas à sua pizza?");
+        System.out.println("1 - Sim");
+        System.out.println("2 - Não");
+
+        int escolha = perguntarCliente.nextInt();
+
+        if (escolha == 1) {
+            System.out.print("Digite o nome do recheio desejado: ");
+            String nomeRecheio = perguntarCliente.next();
+            System.out.println("Bordas recheadas com " + nomeRecheio + " adicionadas à sua pizza!");
+        } else {
+            System.out.println("Bordas recheadas não adicionadas à sua pizza.");
+        }
+    }
 
 }
--- PizzariaBomApetite/src/ifpe/ifpe/PizzaSalgada.java
+++ PizzariaBomApetite/src/ifpe/ifpe/PizzaSalgada.java
@@ -1,6 +1,7 @@
 package ifpe;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class PizzaSalgada extends Pizza {
 	
@@ -8,7 +9,6 @@ public class PizzaSalgada extends Pizza {
 	private List<Ingrediente> ingredientes;
 	private String sabor;
 	private double preco;
-	
 	
 	
 	public String getSabor() {
@@ -36,5 +36,24 @@ public class PizzaSalgada extends Pizza {
 	public List<Ingrediente> getIngredientes() {
 		return ingredientes;
 	}
+	
+	public void adicionarBordasRecheadas() {
+		// TODO Auto-generated method stub
+		Scanner perguntarCliente = new Scanner(System.in);
 
+        System.out.println("Deseja adicionar bordas recheadas à sua pizza?");
+        System.out.println("1 - Sim");
+        System.out.println("2 - Não");
+
+        int escolha = perguntarCliente.nextInt();
+
+        if (escolha == 1) {
+            System.out.print("Digite o nome do recheio desejado: ");
+            String nomeRecheio = perguntarCliente.next();
+            System.out.println("Bordas recheadas com " + nomeRecheio + " adicionadas à sua pizza!");
+        } else {
+            System.out.println("Bordas recheadas não adicionadas à sua pizza.");
+        }
+    }
+	
 }
